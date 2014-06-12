36327f6e38bc1bcfaade4cd245cdbc01dd31ddd5
kelvin <kyu1012@gmail.com>
2014-06-12 16:17:38 -0400

    swap plus-minus buttons

--- mintdesign.js
+++ mintdesign.js
@@ -1,3 +1,18 @@
+$(".faq-heading").click(function () {
+  $header = $(this);
+  //getting the next element
+  $content = $header.next();
+  //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+  $content.slideToggle(500, function () {});
 
+  var imgsrc = $header.find("img").attr('src')
+
+  if(imgsrc === 'plus.svg'){
+    $header.find("img").attr('src', 'minus.svg')
+  }
+  else {
+    $header.find("img").attr('src', 'plus.svg')
+  }
+});
 
 
